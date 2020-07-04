b8d4c44c270dff18ef4d94e60f64453d7bfb33ba
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-07-04 23:21:12 +0900

    【new】グループ編集機能

--- src/main/java/com/example/controller/group/GroupEditController.java
+++ src/main/java/com/example/controller/group/GroupEditController.java
@@ -1,7 +1,18 @@
 package com.example.controller.group;
 
+import java.text.Normalizer.Form;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.domain.Group;
+import com.example.form.GroupForm;
+import com.example.service.group.GroupDetailService;
+import com.example.service.group.GroupEditService;
 
 /**
  * @author oyamadakenji
@@ -13,15 +24,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/group-edit")
 public class GroupEditController {
 	
+	@Autowired
+	private GroupDetailService groupDetailService;
+	
+	@Autowired
+	private GroupEditService groupEditService;
+	
 	/**
 	 * グループ登録画面へ.
 	 * 
 	 * @return グループ登録画面
 	 */
 	@RequestMapping("/to-group-edit")
-	public String toGroupEdit() {
+	public String toGroupEdit(Integer id,Model model) {
 		
+		Group group = groupDetailService.load(id);
+		
+		model.addAttribute("groupForm",group);
 		return "group/group_edit";
+	}
+
+	/**
+	 * グループを編集する.
+	 * 
+	 * @param groupForm 入力された新しいグループの情報
+	 * @param redirectAttributes フラッシュスコープ
+	 * @return グループ詳細画面
+	 */
+	@RequestMapping("/group-edit")
+	public String groupEdit(GroupForm groupForm,RedirectAttributes redirectAttributes) {
+		
+		Group group = groupDetailService.load(groupForm.getIntId());
+		
+		BeanUtils.copyProperties(groupForm, group);
+		group.setId(groupForm.getIntId());
+		group.setOwnerId(groupForm.getIntOwnerId());
+		
+		groupEditService.save(group);
+		
+		redirectAttributes.addAttribute("id",groupForm.getIntId());
+		return "redirect:/group-detail/to-group-detail";
 	}
 
 }
--- src/main/java/com/example/repository/group/GroupRepository.java
+++ src/main/java/com/example/repository/group/GroupRepository.java
@@ -105,16 +105,16 @@ public class GroupRepository {
 			Number key = insert.executeAndReturnKey(param);
 			group.setId(key.intValue());
 		} else {
-//        	StringBuilder sql=new StringBuilder();
-//                //■①INNER JOIN > 左と右のテーブルがあるのが前提 > 無いとエラー
-//                //  ②LEFT OUTER JOIN > 基本LEFT OUTER JOINでOK > 左側に右側をくっつける > 右なくてもエラーにならない。
-//                sql.append("UPDATE orders SET user_id=:userId,status=:status,total_price=:totalPrice,order_date=:orderDate,");
-//                sql.append("destination_name=:destinationName,destination_email=:destinationEmail,destination_zipcode=:destinationZipcode,");
-//                sql.append("destination_address=:destinationAddress,destination_tel=:destinationTel,delivery_time=:deliveryTime,");
-//                sql.append("payment_method=:paymentMethod WHERE id=:id");
-//
-//                //■ 「sql.toString()」に変更注意
-//                template.update(sql.toString(), param);
+			StringBuilder sql = new StringBuilder();
+
+			sql.append("UPDATE ");
+			sql.append(" groups ");
+			sql.append("SET ");
+			sql.append(" id=:id, name=:name, owner_id=:ownerId ");
+			sql.append("WHERE ");
+			sql.append(" id =:id ");
+
+            template.update(sql.toString(), param);
 		}
 		return group;
 	}
