000382c12a61bebaf4ea887cac20cea8dec56c97
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-08-14 14:23:56 +0900

    【modify】タスク編集画面にバリデーションの実装

--- src/main/java/com/example/controller/task/TaskEditController.java
+++ src/main/java/com/example/controller/task/TaskEditController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -47,8 +49,12 @@ public class TaskEditController {
 	}
 
 	@RequestMapping("/task-edit")
-	public String taskEdit(TaskForm form,RedirectAttributes redirectAttributes) {
+	public String taskEdit(@Validated TaskForm form, BindingResult result, RedirectAttributes redirectAttributes) {
 
+		if (result.hasErrors()) {
+			return "task/task_edit";
+		}		
+		
 		Task task = new Task();
 		BeanUtils.copyProperties(form, task);
 		task.setId(form.getIntId());
